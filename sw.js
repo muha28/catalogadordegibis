eb0492424b22d18e107064b4af14d52618ebfb92
muha28 <45806901+muha28@users.noreply.github.com>
2026-09-07 15:07:35 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker básico para ativar o prompt de instalação PWA
 
-const CACHE_NAME = 'gibis-app-v1';
+const CACHE_NAME = 'gibis-app-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
